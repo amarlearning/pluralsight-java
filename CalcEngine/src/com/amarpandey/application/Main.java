aee210e7b7f1e74727dfebeef2129b83081601b1
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-02-01 23:47:56 +0530

    added new error test case

--- CalcEngine/src/com/amarpandey/application/Main.java
+++ CalcEngine/src/com/amarpandey/application/Main.java
@@ -8,7 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 
 		String[] statements = {
-					"add 10.0", 
+					"add 10.0",
+					"addx 10.0 aa",
 					"add aa 20.0", 
 					"addx 10.0 20.0",
 					"add 10.0 20.0", 
